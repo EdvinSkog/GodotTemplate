c4d494c21db345a7335b3d633abf3853e49f01eb
Ed <cooljoller@gmail.com>
2024-06-23 18:14:07 +0200

    debug ui

---
+++ scenes/user_interface/debug_interface.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+@export var enable_debug_ui: bool = false
+@export var debug_labels: Array[String]
+
+@onready var cheats: MenuButton = $VBoxContainer/MenuButton
+@onready var cheats_popup: PopupMenu = cheats.get_popup()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if(enable_debug_ui):
+		visible = true
+	else:
+		process_mode = Node.PROCESS_MODE_DISABLED
+		visible = false
+	cheats_popup.id_pressed.connect(check_popup_id)
+
+func check_popup_id(id):
+	print(id)
+	match id:
+		0:
+			print("Cheat 0")
+		1:
+			print("Cheat 1")
+		2:
+			print("Cheat 2")
---
+++ scenes/user_interface/debug_label.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@export_enum("GameManager", "PlayerVariables") var what_script = 0
+@export var what_property: String
+@export var custom_text: String
+var selected_script
+var expression = Expression.new()
+
+@onready var label_value: Label = $LabelValue
+@onready var label_property: Label = $LabelProperty
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	label_property.text = what_property + "="
+	if(!custom_text.is_empty()):
+		label_property.text = custom_text + "="
+	match what_script:
+		0:
+			selected_script = GameManager
+		1:
+			selected_script = PlayerVariables
+	expression.parse(what_property)
+
+func _process(delta):
+	var result = expression.execute([], selected_script)
+	label_value.text = str(result)
