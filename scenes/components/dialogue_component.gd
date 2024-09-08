c51b82b95a5bf5eb8efed7a94d1b8e55a1fb976e
Ed <cooljoller@gmail.com>
2024-09-08 16:09:13 +0200

    Update dialogue_component.gd

--- scenes/components/dialogue_component.gd
+++ scenes/components/dialogue_component.gd
@@ -3,20 +3,29 @@ extends Node
 const Balloon = preload("res://scenes/user_interface/dialogue_balloon.tscn") # UI element
 @export var dialogue_resource: DialogueResource
 
+signal dialogue_finished
+signal title_finished(title: String)
+
+
 func _ready():
 	# Signal that is triggered when dialogue finishes
-	DialogueManager.dialogue_ended.connect(dialogue_finished)
+	DialogueManager.dialogue_ended.connect(_dialogue_finished)
+	DialogueManager.passed_title.connect(_title_finished)
 
 
 func play_dialogue(dialogue_part: String):
 	var balloon: Node = Balloon.instantiate()
+	GameManager.current_dialogue_balloon = balloon
 	get_tree().current_scene.add_child(balloon)
 	balloon.start(dialogue_resource, dialogue_part)
 
-func dialogue_finished(used_resource):
-	if (used_resource == dialogue_resource): 
-		# Code for finished dialogue
-		pass
+func _title_finished(title: String):
+	emit_signal("title_finished")
+
+func _dialogue_finished(used_resource):
+	if (used_resource == dialogue_resource):
+		# Capture Mouse? Allow Movement?
+		emit_signal("dialogue_finished")
 
 func _on_timer_timeout():
 	play_dialogue("start")
