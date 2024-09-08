980542acef6419bb33a697ce751945df312de493
Ed <cooljoller@gmail.com>
2024-09-08 14:36:54 +0200

    loading screen styles

--- scenes/global/scene_manager.gd
+++ scenes/global/scene_manager.gd
@@ -7,8 +7,10 @@ signal level_changed
 signal progress_changed(progress)
 signal load_done
 
-var _load_screen_path : String = "res://scenes/user_interface/loading_screen.tscn"
-var _load_screen = load(_load_screen_path)
+var _load_screen_path_normal : String = "res://scenes/user_interface/loading_screen.tscn"
+var _load_screen = load(_load_screen_path_normal)
+var _load_screen_path_subtle : String = "res://scenes/user_interface/loading_screen_subtle.tscn"
+var _load_screen_subtle = load(_load_screen_path_subtle)
 var _loaded_resource:  PackedScene
 var _scene_path: String
 var _progress: Array = []
@@ -19,30 +21,46 @@ func remove_dialogue_balloon():
 	if(GameManager.current_dialogue_balloon):
 		GameManager.current_dialogue_balloon.queue_free()
 
-func load_scene(scene_path: String, style : int = 0) -> void:
+func load_scene(scene_path: String, speed_multipler: float = 1, style : String = "normal") -> void:
 	remove_dialogue_balloon()
 	
-	if (style == 1):
-		switch_scene(scene_path)
-		return
 	
-	
+
 	_scene_path = scene_path
 	
-	var new_loading_screen: LoadingScreen = _load_screen.instantiate()
+	# Spawn loading screen
+	var new_loading_screen: LoadingScreen
+	match style:
+		"normal":
+			new_loading_screen = _load_screen.instantiate()
+		"subtle":
+			new_loading_screen = _load_screen_subtle.instantiate()
+		_:
+			new_loading_screen = _load_screen.instantiate()
+
 	get_tree().get_root().add_child(new_loading_screen)
 	
+	# Set speed multipler
+	new_loading_screen.animation_player.speed_scale = speed_multipler
+	
+	# Set-up signal connections for loading screen
 	self.progress_changed.connect(new_loading_screen._update_progress_bar)
 	self.load_done.connect(new_loading_screen._start_outro_animation)
 	
+	# Wait until loading screen have fully covered the screen
 	await Signal(new_loading_screen, "loading_screen_has_full_coverage")
+
+	# Start loading the next scene
 	start_load()
 	await Signal(self, "load_done")
+
+	# Delete the old scene.
 	if(current_level != null):
 		current_level.queue_free() # Remove current level
 	await Signal(new_loading_screen, "finished_waiting")
 	current_level = _loaded_resource.instantiate() # Set the new one as current
-	level_changed.emit() # Adds new level in App script
+	
+	emit_signal("level_changed") # Adds new level in App script
 
 
 func start_load() -> void:
--- scenes/user_interface/loading_screen.gd
+++ scenes/user_interface/loading_screen.gd
@@ -1,5 +1,6 @@
 class_name LoadingScreen extends CanvasLayer
 
+## Screen is fully covered by the Loading Screen.
 signal loading_screen_has_full_coverage
 ## This signal means the loading screen wants to instantiate the new scene.
 signal finished_waiting
@@ -11,12 +12,19 @@ signal finished_waiting
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var progress_bar : ProgressBar = $Panel/ProgressBar
 
-func _update_progress_bar(new_value: float) -> void:
-	progress_bar.set_value_no_signal(new_value * 100)
+func _ready() -> void:
+	_reveal_loading_screen()
+
+func _reveal_loading_screen() -> void:
+	animation_player.play("fade_in")
+	await Signal(animation_player, "animation_finished")
+	emit_signal("loading_screen_has_full_coverage") # Can also be called within the animationplayer
+	_start_loading_animation()
+
+func _start_loading_animation() -> void:
+	animation_player.queue("start_load")
 
 func _start_outro_animation() -> void:
-	await Signal(animation_player, "animation_finished")
-	
 	if(await_player_input):
 		$Panel/LabelPress.visible = true
 		await Signal(self, "pressed_key")
@@ -24,8 +32,11 @@ func _start_outro_animation() -> void:
 	emit_signal("finished_waiting")
 	if(play_end_animation):
 		animation_player.queue("end_load")
-		await Signal(animation_player, "animation_finished")
+	await Signal(animation_player, "animation_finished") # Can be the loading-animation, not always outro-animation
 	self.queue_free()
+
+func _update_progress_bar(new_value: float) -> void:
+	progress_bar.set_value_no_signal(new_value * 100)
 
 signal pressed_key
 
