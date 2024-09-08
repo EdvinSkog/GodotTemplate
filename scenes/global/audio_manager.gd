24212ed186a5f412d2489bc8020ce5964404ecfe
Ed <cooljoller@gmail.com>
2024-09-08 16:17:31 +0200

    audio manager change

--- scenes/global/audio_manager.gd
+++ scenes/global/audio_manager.gd
@@ -1,19 +1,19 @@
 extends Node
 
 enum AUDIO_TYPE {sfx, music, voice}
-@onready var sfx_scene: PackedScene = load("res://scenes/components/sound_effect.tscn")
+@onready var sfx_scene: PackedScene = load("res://scenes/global/audio/sound_effect.tscn")
 @onready var music = $Music
 @onready var voice = $Voice
 # Global volume parameters
 var volume_sfx = -15
-var volume_music = -15
-var volume_voice = -15
+var volume_music = -10
+var volume_voice = -10
 
 
 # Uses and instantiates the sound_effect.tscn
-func play(stream: AudioStream, local_volume_change: float = 0):
+func play_sfx(path: String, local_volume_change: float = 0):
 	var sfx = sfx_scene.instantiate()
-	sfx.stream = stream
+	sfx.stream = load(path)
 	sfx.volume_db = volume_sfx + local_volume_change
 	add_child(sfx)
 	pass
