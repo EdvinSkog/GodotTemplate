cec5d677140a5a94aedc6534628d79b00071521d
Ed <cooljoller@gmail.com>
2024-09-08 16:49:49 +0200

    menu buttons

---
+++ scenes/user_interface/menus/title_screen.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_button_start_pressed() -> void:
+	SceneManager.load_scene("res://scenes/level/test_level.tscn", 1, "subtle")
+
+
+func _on_button_settings_pressed() -> void:
+	SceneManager.load_scene("res://scenes/user_interface/menus/settings.tscn", 1, "subtle")
+
+
+func _on_button_credits_pressed() -> void:
+	SceneManager.load_scene("res://scenes/user_interface/menus/credits.tscn", 1, "subtle")
