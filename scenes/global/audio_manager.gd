015cebc195c5ed65a9ba4b46cf14a03da8c2ef95
Ed <cooljoller@gmail.com>
2024-10-17 23:51:19 +0200

    music fading

--- scenes/global/audio_manager.gd
+++ scenes/global/audio_manager.gd
@@ -1,91 +1,95 @@
 extends Node
 
-enum AUDIO_TYPE {master, sfx, music, ambience, voice}
 @onready var sfx_scene: PackedScene = load("res://scenes/global/audio/sound_effect.tscn")
-@onready var music = $Music
+@onready var music_player = $Music
 @onready var voice = $Voice
 @onready var ambience = $Voice
 
 @export_dir var music_folder_path: String = "res://assets/audio/music"
 var songlist: Dictionary = {}  # Dictionary to store song references by name
 
+
+
+
 func _ready() -> void:
 	load_music_files()
 
-func load_music_files():
+func load_music_files(folder_path: String = ""):
 	# Standard
-	var dir: DirAccess = DirAccess.open(music_folder_path)
-	dir.list_dir_begin()  # Start reading the directory
-	var file_name = dir.get_next()
-	while file_name != "":
-		if file_name.ends_with(".wav") or file_name.ends_with(".mp3") or file_name.ends_with(".ogg"):  # Check for valid music files
-			var song_name = file_name.get_basename()  # Get the file name without extension
-			songlist[song_name] = music_folder_path + "/" + file_name  # Add to the dictionary
-		file_name = dir.get_next()
-	dir.list_dir_end()  # Close directory
-	print("Songlist: ", songlist)
-
+	var dir: DirAccess = DirAccess.open(music_folder_path + folder_path)
+	if dir:
+		dir.list_dir_begin()  # Start reading the directory
+		var file_name = dir.get_next()
+		while file_name != "":
+			# Ignore "." and ".." which refer to the current and parent directory
+			if file_name != "." and file_name != "..":
+				var full_path = music_folder_path + folder_path + "/" + file_name
+				if dir.current_is_dir():
+					# Recursively load music files from subdirectories
+					load_music_files(folder_path + "/" + file_name)
+				elif file_name.ends_with(".wav") or file_name.ends_with(".mp3") or file_name.ends_with(".ogg"):
+					var song_name = file_name.get_basename()  # Get the file name without extension
+					songlist[song_name] = full_path  # Add full path to the dictionary
+			file_name = dir.get_next()
+		
+		dir.list_dir_end()  # Close directory
+	else:
+		print("Error: Could not open folder at path:", music_folder_path + folder_path)
+	#print("Song list is ", songlist)
 
 # Uses and instantiates the sound_effect.tscn
-func play_sfx(path: String, local_volume_change: float = 0):
+func play_sfx(path: String, volume_modifier: float = 0):
 	var sfx = sfx_scene.instantiate()
 	sfx.stream = load(path)
-	sfx.volume_db = local_volume_change
+	sfx.volume_db = 0 + volume_modifier
 	add_child(sfx)
 
 # Uses paths, not AudioStream.
-func play_music(song_name: String, local_volume_change: float = 0):
+func play_music(song_name: String, volume_modifier: float = 0, fade_time = 0.1):
 	var song_path = songlist.get(song_name, null)
 	if song_path:
-		print(song_path)
 		var music_stream = load(song_path)
 		if music_stream:
-			if music.stream:
-				music.stop()  # Stop any currently playing music
-			music.stream = music_stream
-			music.play()
+			if music_player.stream:
+				music_player.stop()  # Stop any currently playing music
+			
+			music_player.stream = music_stream
+			
+			music_player.volume_db = -40
+			music_player.play()
+			var tween = get_tree().create_tween()
+			tween.set_ease(Tween.EASE_OUT)
+			tween.tween_property(music_player, "volume_db", 0 + volume_modifier, fade_time)
 		else:
 			print("Error: Could not load the music stream for song:", song_name)
 	else:
 		print("Error: Song name not found in songlist:", song_name)
 
-func play_ambience(path: String, local_volume_change: float = 0):
+func play_ambience(path: String, volume_modifier: float = 0):
 	ambience.stream = load(path)
-	ambience.volume_db = local_volume_change
+	ambience.volume_db = 0 + volume_modifier
 	ambience.play()
 
 #This function uses paths, because of how Dialogue Manager works
-func play_voice(path: String, local_volume_change: float = 0):
+func play_voice(path: String, volume_modifier: float = 0):
 	voice.stream = load(path)
-	voice.volume_db = local_volume_change
+	voice.volume_db = 0 + volume_modifier
 	voice.play()
 
 func stop_music(fade_time: float = 0.1):
 	var tween = get_tree().create_tween()
-	tween.tween_property(music, "volume_db", -30, fade_time)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(music_player, "volume_db", -40, fade_time)
 	await tween.finished
-	music.stop()
+	music_player.stop()
 
-func set_global_volume(option: AUDIO_TYPE, amount: float):
+func set_global_volume(bus_name: String, amount: float):
+	var bus_index = AudioServer.get_bus_index(bus_name)
 	amount = linear_to_db(amount)
-	match option:
-		AUDIO_TYPE.master:
-			AudioServer.set_bus_volume_db(0, amount)
-		AUDIO_TYPE.sfx:
-			AudioServer.set_bus_volume_db(1, amount)
-		AUDIO_TYPE.sfx:
-			AudioServer.set_bus_volume_db(1, amount)
-		AUDIO_TYPE.music:
-			AudioServer.set_bus_volume_db(2, amount)
-		AUDIO_TYPE.ambience:
-			AudioServer.set_bus_volume_db(3, amount)
-		AUDIO_TYPE.voice:
-			AudioServer.set_bus_volume_db(4, amount)
-		_:
-			print_debug("Invalid audio type")
+	AudioServer.set_bus_volume_db(bus_index, amount)
 
 # Increases/decreases by db amount
 func change_music_volume(db, fade_time: float = 0):
-	db = music.volume_db + db
+	db = 0 + db
 	var tween = get_tree().create_tween()
-	tween.tween_property(music, "volume_db", db, fade_time)
+	tween.tween_property(music_player, "volume_db", db, fade_time)
--- scenes/user_interface/menus/settings.gd
+++ scenes/user_interface/menus/settings.gd
@@ -1,6 +1,7 @@
 extends Control
 
-
+func _ready() -> void:
+	AudioManager.stop_music()
 
 func _on_button_return_pressed() -> void:
 	SceneManager.load_scene("res://scenes/user_interface/menus/title_screen.tscn", 1, "subtle")
