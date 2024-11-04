8a783fda1640a73ae3397fda16d3d7fe260ef741
Ed <cooljoller@gmail.com>
2024-11-04 17:10:02 +0100

    Main export current level

--- scenes/level/main.gd
+++ scenes/level/main.gd
@@ -1,10 +1,16 @@
 extends Node
 
+@export var override_title_screen_start: PackedScene = load("res://scenes/user_interface/menus/title_screen.tscn")
+
 @onready var level_holder = $SubViewportContainer/SubViewport/LevelHolder
 @onready var gameplay_ui = $CanvasLayer/GameplayInterface
 
 ## Setup
 func _ready():
+	assert(override_title_screen_start != null)
+	var override_title_screen_start = override_title_screen_start.instantiate()
+	level_holder.add_child(override_title_screen_start)
+	
 	var current_level = level_holder.get_child(0)
 	SceneManager.set_current_level(current_level)
 	_connect_signals()
