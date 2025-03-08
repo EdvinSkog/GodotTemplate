94a1a6c2845c25b327c659dc6c213ae17640c73b
Ed <cooljoller@gmail.com>
2025-03-08 14:06:15 +0100

    small cleanup
    Moved Main tscn, removed aim controller, autoload cleaning

--- scenes/global/game_manager.gd
+++ scenes/global/game_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var game_time: float = 0
-var current_dialogue_balloon
+var current_dialogue_balloon: DialogueBalloon
 
 func _on_timer_second_timeout():
 	game_time += 1
--- scenes/global/player_manager.gd
+++ scenes/global/player_manager.gd
@@ -4,11 +4,5 @@ extends Node
 
 signal active_changed(active: bool)
 
-var aiming_target # For aim_controller
-
-# Movement
-var _allow_move: bool = true
-
-func set_player_active(option: bool): # Disable movement/input etc.
-	_allow_move = option
-	emit_signal("active_changed", option)
+## Reference to Player node
+var player: Player
