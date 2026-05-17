3e7ead28abeaa8e6fa1b1bb0223d3bb9c9dc76f4
Ed <cooljoller@gmail.com>
2026-05-17 08:46:29 +0200

    Collection2D class add

--- src/tools/collection_2d.gd
+++ src/tools/collection_2d.gd
@@ -1,10 +1,10 @@
 @tool
-@icon("res://assets/icons/editor/folder_3d.svg")
-class_name Collection3D extends Node3D
+@icon("res://assets/icons/editor/folder_2d.svg")
+class_name Collection2D extends Node2D
 
 @export_tool_button("Store Position", "EditorPosition") var action_store := do_action_store
-@export var stored_position: Vector3
-@export_tool_button("Move Children to Stored Position", "3D") var action_move := do_action_move
+@export var stored_position: Vector2
+@export_tool_button("Move Children to Stored Position", "2D") var action_move := do_action_move
 @export_category("Settings")
 @export var disable_process_when_hidden: bool = false
 
@@ -14,7 +14,7 @@ func do_action_store() -> void:
 
 func do_action_move() -> void:
 	for node in get_children():
-		if node is Node3D:
+		if node is Node2D:
 			node.global_position = stored_position
 
 func _ready() -> void:
@@ -26,9 +26,10 @@ func _ready() -> void:
 ## Force the node's name to be uppercase. 
 # Personal preference, helps distinguish them from normal Node3D
 func _on_renamed() -> void:
-	if name.to_upper() != name:
-		name = name.to_upper()
-	if name.to_upper() != name and name != "Collection3D":
+	if name.is_empty():
+		name = "Collection2D"
+		return
+	if name.to_upper() != name and name != "Collection2D":
 		name = name.to_upper()
 
 func _on_visibility_changed() -> void:
